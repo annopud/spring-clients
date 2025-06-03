afe07ecaf283ac19a31a317a44282cc19b3ea776
tttmb <annop.email@gmail.com>
2025-06-03 23:54:44 +0700

    Refactor the code.

--- src/test/java/dev/annopud/spring_clients/SpringClientsApplicationTests.java
+++ src/test/java/dev/annopud/spring_clients/SpringClientsApplicationTests.java
@@ -8,6 +8,9 @@ class SpringClientsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// This test is used to check if the Spring application context loads successfully.
+		// If there are any issues with the configuration or beans, this test will fail.
+		// No additional assertions are needed here.
 	}
 
 }
