ac5210d8c8ac789448055c5f677916008a8e34a1
tttmb <annop.email@gmail.com>
2025-06-09 23:42:46 +0700

    Add Docker support and convert application properties to YAML format

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+FROM maven:3.9.9-eclipse-temurin-21-alpine AS build
+
+ENV TZ=Asia/Bangkok
+
+WORKDIR /app
+
+COPY settings.xml /root/.m2/settings.xml
+
+COPY . .
+
+RUN mvn clean package -Dmaven.test.skip=true
+
+FROM eclipse-temurin:21.0.7_6-jre-noble
+ENV TZ=Asia/Bangkok
+
+USER root
+
+RUN useradd -ms /bin/bash appuser && mkdir -p /app && chown appuser:appuser /app
+
+COPY --from=build /app/target/spring-clients.jar /app/app.jar
+
+RUN chown appuser:appuser /app/app.jar
+
+USER appuser
+
+EXPOSE 8080
+
+ENTRYPOINT ["java","-jar","/app/app.jar"]
--- src/test/java/dev/annopud/spring_clients/todo/TodoClientTest.java
+++ src/test/java/dev/annopud/spring_clients/todo/TodoClientTest.java
@@ -1,11 +1,15 @@
 package dev.annopud.spring_clients.todo;
 
 import org.assertj.core.api.Assertions;
+import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TodoClientTest {
@@ -17,6 +21,15 @@ class TodoClientTest {
     void findAll() {
         List<Todo> todos = todoClient.findAll();
         Assertions.assertThat(todos).hasSize(200);
+    }
+
+    @Test
+    void findAllTest() {
+        AtomicBoolean b = new AtomicBoolean(false);
+        CompletableFuture.runAsync(() -> b.set(true));
+        Awaitility.await().atMost(5, TimeUnit.SECONDS).until(
+            b::get
+        );
     }
 
 }
